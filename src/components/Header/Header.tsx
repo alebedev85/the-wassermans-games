34690d203be87a88f1448e9e3a24ec7ab2c9d2e2
Andrey Lebedev <alebedev424@gmail.com>
2025-10-14 13:51:45 +0300

    featur: убрал лишнее

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,6 @@
-import { FaUserCircle } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store";
 import { toggleTheme } from "../../store/themeSlice";
-import LoginForm from "../LoginForm/LoginForm";
 import styles from "./Header.module.scss";
 
 const Header = () => {
@@ -15,20 +13,11 @@ const Header = () => {
         <p className={styles.subtitle}>Расписание игр</p>
         <div className={styles.actions}>
           <button
-            className={styles.buttonLogin}
-            onClick={() => console.log("login")}
-          >
-            <FaUserCircle />
-          </button>
-          <button
             className={styles.buttonTheme}
             onClick={() => dispatch(toggleTheme())}
           >
             {theme === "light" ? "☀️" : "🌙"}
           </button>
-          <div className={styles.dropdownMenu}>
-            <LoginForm />
-          </div>
         </div>
       </div>
     </header>
